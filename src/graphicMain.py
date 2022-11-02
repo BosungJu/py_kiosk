0a8adcc10089bfd2edcdda41377d3e3b63db405b
BOSEONG\joobs <joobs1127@gmail.com>
2022-11-02 13:28:51 +0900

    edit  TODO

--- src/graphicMain.py
+++ src/graphicMain.py
@@ -3,28 +3,27 @@ from __ProductManager import instance
 from PyQt5.QtWidgets import *
 from PIL import Image
 
+
 class Graphic(QDialog):
     def __init__(self):
         super().__init__()
         self.__initUI()
 
     def __getCategory(self):
-        '''get category data'''
+        """get category data"""
         self.category = []
-        f = open('../res/data/category.txt', 'r')
+        f = open('../res/data/category.txt', 'r') # TODO change DB
 
         for s in f.readlines():
             self.category.append(s)
 
-
     def moveSide(self, isLeft: bool):
-        '''moveSide(isLeft : boolean) -> None
-        isLeft는 왼쪽 버튼 눌렀을 때 true'''
+        """moveSide(isLeft : boolean) -> None
+        isLeft는 왼쪽 버튼 눌렀을 때 true"""
         # TODO go left or right widget
         pass
 
-
-    def generateButton(self, product : Product):
+    def generateButton(self, product: Product):
         # TODO return Button
         raise NotImplemented('generateButton func in Graphic class')
 
